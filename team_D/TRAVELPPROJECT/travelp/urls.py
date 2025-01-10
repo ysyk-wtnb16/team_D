28c3e1191099fde9a87e252da9a165eb891c0586
moppanch <oom2325009@stu.o-hara.ac.jp>
2025-01-10 16:32:39 +0900

    あ

--- team_D/TRAVELPPROJECT/travelp/urls.py
+++ team_D/TRAVELPPROJECT/travelp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'travelp'
 
 urlpatterns = [
-    path('home', views.IndexView.as_view(), name='home'),
+    path('', views.IndexView.as_view(), name='home'),
 
     path('search', views.SearchView.as_view(), name='search'),
 
