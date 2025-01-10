241fcfd1bdd7aa359d98d31f705e51d415c7db40
ninomiya22222 <mochi.0304_2005@icloud.com>
2025-01-10 14:28:52 +0900

    jo

--- team_D/TRAVELPPROJECT/travelp/urls.py
+++ team_D/TRAVELPPROJECT/travelp/urls.py
@@ -5,4 +5,6 @@ app_name = 'travelp'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+
+    path('/search', views.SearchView.as_view(), name='search'),
 ]
--- team_D/TRAVELPPROJECT/travelp/views.py
+++ team_D/TRAVELPPROJECT/travelp/views.py
@@ -5,3 +5,7 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
+class SearchView(TemplateView):
+
+    template_name = 'search.html'
