a144e5ae070cf8a37caaa9e7cac6694870e75fe4
moppanch <oom2325009@stu.o-hara.ac.jp>
2025-01-14 16:36:34 +0900

    m

--- team_D/TRAVELPPROJECT/travelp/views.py
+++ team_D/TRAVELPPROJECT/travelp/views.py
@@ -29,3 +29,23 @@ class MyplanView(TemplateView):
 class PayView(TemplateView):
 
     template_name = 'pay.html'
+
+class S_homeView(TemplateView):
+
+    template_name = 's_home.html'
+
+class S_sinseiView(TemplateView):
+
+    template_name = 's_sinsei.html'
+
+class S_pageView(TemplateView):
+
+    template_name = 's_page.html'
+
+class S_postView(TemplateView):
+
+    template_name = 's_post.html'
+
+class S_historyView(TemplateView):
+
+    template_name = 's_history.html'
