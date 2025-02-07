4a61d6e4da35df0ceba000d410c393ba44581a1e
KatouKeigo <k.keigo.k@icloud.com>
2025-02-07 14:11:28 +0900

    パスワード変更機能

--- team_D/TRAVELPPROJECT/accounts/forms.py
+++ team_D/TRAVELPPROJECT/accounts/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.forms import CheckboxSelectMultiple, EmailInput
 # models.pyで定義したカスタムUserモデルをインポート
 from .models import CustomUser
+from django.contrib.auth.forms import PasswordChangeForm
+from django import forms
+from django.core.exceptions import ValidationError
+
 
 class CustomUserCreationForm(UserCreationForm):
     '''UserCreationFormのサブクラス
@@ -29,3 +33,13 @@ class LoginForm(AuthenticationForm):
         self.fields["username"].widget.attrs["class"] = "form-control"
 
         self.fields["password"].widget.attrs["class"] = "form-control"
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+   old_password = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}))
+   new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
+   new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
+   def clean_new_password1(self):
+       new_password = self.cleaned_data.get('new_password1')
+       if len(new_password) < 8:
+          raise ValidationError("パスワードは8文字以上である必要があります。")
+       return new_password
