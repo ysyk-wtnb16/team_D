2d28b359cfb0530783600d87caa5b9ee2c7cf6e8
ninomiya22222 <mochi.0304_2005@icloud.com>
2025-02-07 11:04:31 +0900

    コミット

--- team_D/TRAVELPPROJECT/travelp/models.py
+++ team_D/TRAVELPPROJECT/travelp/models.py
@@ -124,3 +124,4 @@ class FundraisingProject(models.Model):
     goal_amount = models.DecimalField(max_digits=10, decimal_places=2)
     collected_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
