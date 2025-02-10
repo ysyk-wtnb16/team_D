afd95b2113ace3443c55b7d516c167546cd7fc72
ysyk-wtnb16 <wtnb16ysyk812@gmail.com>
2025-02-10 11:40:25 +0900

    プロフィール関連の変更

--- team_D/TRAVELPPROJECT/accounts/urls.py
+++ team_D/TRAVELPPROJECT/accounts/urls.py
@@ -1,50 +1,33 @@
 from django.urls import path
-# viewsモジュールをインポート
-from . import views
-# viewsをインポートしてauth_viewという記名で利用する
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LogoutView
+from .views import (
+    SignUpView, SignUpSuccessView, LoginView, logout_view,
+    delete_account, password_change_view, password_change_done_view,
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+)
 
-# URLパターンを逆引きできるように名前を付ける
 app_name = 'accounts'
 
-# URLパターンを登録するための変数
 urlpatterns = [
-    # サインアップページのビューの呼び出し
-    # 「http(s)://<ホスト名>/signup/」へのアクセスに対して、
-    # viewsモジュールのSignUpViewをインスタンス化する
-    path('signup/',
-         views.SignUpView.as_view(),
-         name='signup'),
+    # サインアップ
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/success/', SignUpSuccessView.as_view(), name='signup_success'),
     
-    # サインアップ完了ページのビューの呼び出し
-    # 「http(s)://<ホスト名>/signup_success/」へのアクセスに対して、
-    # viewsモジュールのSignUpSuccessViewをインスタンス化する
-    path('signup_success/',
-         views.SignUpSuccessView.as_view(),
-         name='signup_success'),
+    # ログイン
+    path('login/', LoginView.as_view(), name='login'),
     
-    # ログインページの表示
-    # 「http(s)://<ホスト名>/signup/」へのアクセスに対して、
-    # django.contrib.auth.views.LoginViewをインスタンス化して
-    # ログインページを表示する
-    path('login/',
-         # ログイン用のテンプレート(フォーム)をレンダリング
-         auth_views.LoginView.as_view(template_name='login.html'),
-         name='login'
-         ),
+    # ログアウト
+    path('logout/', logout_view, name='logout'),
     
-    # ログアウトを実行
-    # 「http(s)://<ホスト名>/logout/」へのアクセスに対して、
-    # django.contrib.auth.views.logoutViewをインスタンス化して
-    # ログアウトさせる
-    path('logout/', views.logout_view, name='logout'),
+    # アカウント削除
+    path('delete_account/', delete_account, name='delete_account'),
+    
+    # パスワード変更
+    path('password_change/', password_change_view, name='password_change'),
+    path('password_change_done/', password_change_done_view, name='password_change_done'),
 
-    path('delete_account/', views.delete_account, name='delete_account'),
-
-      path('password_change/', views.password_change_view, name='password_change'),
-    
-    path('password_change/done/', views.password_change_done_view, name='password_change_done')
-
-    
+    # パスワードリセット関連
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
